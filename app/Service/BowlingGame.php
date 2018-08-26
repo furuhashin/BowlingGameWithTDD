72e03822c2ee2ca2a8d3a5a1eceace37bdfecceb
nob <yasai.saita@gmail.com>
2018-08-26 23:46:58 +0900

    [第４章　完了]

--- app/Service/BowlingGame.php
+++ app/Service/BowlingGame.php
@@ -16,11 +16,28 @@ class BowlingGame
         $this->spare = false;
         $this->last_pins = 0;
         $this->shot_no = 1;
+        $this->strike_bonus_count = 0;
+        $this->double_bonus_count = 0;
     }
 
     public function record_shot($pins)
     {
         $this->score += $pins;
+
+        $this->calc_spare_bonus($pins);
+
+        $this->calc_strike_bonus($pins);
+
+        $this->last_pins = $pins;
+
+        $this->proceed_next_shot();
+    }
+
+    /**
+     * @param $pins
+     */
+    public function calc_spare_bonus($pins): void
+    {
         if ($this->spare) {
             $this->score += $pins;
             $this->spare = false;
@@ -28,7 +45,32 @@ class BowlingGame
         if ($this->shot_no == 2 && $this->last_pins + $pins == 10) {
             $this->spare = true;
         }
-        $this->last_pins = $pins;
+    }
+
+    /**
+     * @param $pins
+     */
+    public function calc_strike_bonus($pins): void
+    {
+        if ($this->strike_bonus_count > 0) {
+            $this->score += $pins;
+            $this->strike_bonus_count--;
+        }
+        if ($this->double_bonus_count > 0) {
+            $this->score += $pins;
+            $this->double_bonus_count--;
+        }
+        if ($pins == 10) {
+            if ($this->strike_bonus_count == 0) {
+                $this->strike_bonus_count = 2;
+            } else {
+                $this->double_bonus_count = 2;
+            }
+        }
+    }
+
+    public function proceed_next_shot(): void
+    {
         if ($this->shot_no == 1) {
             $this->shot_no = 2;
         } else {
--- tests/Unit/BowlingGameTest.php
+++ tests/Unit/BowlingGameTest.php
@@ -53,6 +53,47 @@ class BowlingGameTest extends TestCase
         $this->assertSame(13,$this->game->score);
     }
 
+    public function testストライクをとると次の２球分のピン数を加算()
+    {
+        $this->game->record_shot(10);
+        $this->game->record_shot(3);
+        $this->game->record_shot(3);
+        $this->game->record_shot(1);
+        $this->record_many_shots(15,0);
+        $this->assertSame(23,$this->game->score);
+    }
+
+    public function test連続ストライクすなわちダブル()
+    {
+        $this->game->record_shot(10); //ここでのボーナスは10+3 2
+        $this->game->record_shot(10); //ここでのボーナスは3+1  1 2
+        $this->game->record_shot(3); // 0 1
+        $this->game->record_shot(1); // 0 0
+        $this->record_many_shots(15,0);
+        $this->assertSame(41,$this->game->score); //ボーナス+実際に倒したピン数 = 17+24= 41
+    }
+
+    public function test3連続ストライクすなわちターキー()
+    {
+        $this->game->record_shot(10); //ここでのボーナスは10+10 2 strike_bonus_countに２が入る
+        $this->game->record_shot(10); //ここでのボーナスは10+3  1 2 double_bonus_countに２が入る
+        $this->game->record_shot(10); // ここでのボーナスは3+1  0 1 2 strike_bonus_countに２が入る
+        $this->game->record_shot(3);
+        $this->game->record_shot(1);
+        $this->record_many_shots(12,0);
+        $this->assertSame(71,$this->game->score); //ボーナス+実際に倒したピン数 = 37+34= 71
+    }
+
+//    public function test1投目は０で二球目で10ピン()
+//    {
+//        $this->game->record_shot(0);
+//        $this->game->record_shot(10);
+//        $this->game->record_shot(3);
+//        $this->game->record_shot(1);
+//        $this->record_many_shots(16,0);
+//        $this->assertSame(17,$this->game->score);
+//    }
+
     public function record_many_shots($count,$pins): void
     {
         for ($i = 1; $i <= $count; $i++) {
